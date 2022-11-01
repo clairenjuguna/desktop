6bea73dc8168dee3a451a21648907381e0f7fc5a
clairenjuguna <109039274+clairenjuguna@users.noreply.github.com>
2022-11-01 09:54:18 +0300

    desktop

---
+++ resources/views/auth/template.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8" />
+    <title>HUD | Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <link href="assets/css/vendor.min.css" rel="stylesheet" />
+    <link href="assets/css/app.min.css" rel="stylesheet" />
+  </head>
+  <body class="pace-top">
+    <div id="app" class="app app-full-height app-without-header">
+        @yield('content')
+    </div>
+    <script
+    src="assets/js/vendor.min.js"
+    type="f1da1a853d287072e5a2d84d-text/javascript"
+  ></script>
+  <script
+    src="assets/js/app.min.js"
+    type="f1da1a853d287072e5a2d84d-text/javascript"
+  ></script>
+
+  <script
+    src="../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+    data-cf-settings="f1da1a853d287072e5a2d84d-|49"
+    defer=""
+  ></script>
+  <script
+    defer
+    src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993"
+    integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA=="
+    data-cf-beacon='{"rayId":"762c4093886c9d09","version":"2022.10.3","r":1,"token":"4db8c6ef997743fda032d4f73cfeff63","si":100}'
+    crossorigin="anonymous"
+  ></script>
+</body>
+
+
+</html>
